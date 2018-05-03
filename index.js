8230eb0270f5c7ec31b79dace450687265b39fc1
Robin Mehner <robin@coding-robin.de>
2018-05-03 10:01:40 +0200

    chore: add how to propose section

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ function mainView () {
       ${nav()}
       <main class="ph4">
         ${intro()}
+        ${howToProposeATalk()}
         ${speakers()}
         ${codeOfConduct()}
         ${sponsor()}
@@ -123,6 +124,28 @@ function intro () {
           with people coming to JSConf EU 2018 from all over the world.
 
           We will have talks and plenty of space to meet new people. See you there!
+        </section>
+      </div>
+    </section>
+  `
+}
+
+function howToProposeATalk () {
+  return html`
+    <section class="mt4 mt5-ns">
+      <div class="mw9 center cf">
+        <section class="fn fl-l w-100 w-40-l pr4-l">
+          <h2 class="f3 f1-ns lh-title fw9 mb3 mt0 pt3 bw2">
+            How to propose a talk?
+          </h2>
+        </section>
+        <section class="lh-copy f5 f4-ns fl mt0-l measure">
+          Glad that you're thinking about giving a talk. To propose a talk, please get in touch with us:
+          Open up an issue on <a href="https://github.com/berlinjs/jsconf2018-special" class="black link underline">
+          our repo</a>, send us <a href="mailto:submit@berlinjs.org" class="black link underline">an email</a>,
+          <a href="https://twitter.com/berlinjs" class="black link underline">tweet at us @berlinjs</a> or find us
+          <a href="https://berlinjs-slack.herokuapp.com/" class="black link underline">in our Slack</a>. Anything
+          works. We're happy to have you!
         </section>
       </div>
     </section>
